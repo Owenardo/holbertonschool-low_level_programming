fbefd7e4d37b25fa9782b0b24fbaddbb598c8780
Owenardo <owenardm@gmail.com>
2024-10-17 16:40:19 +0000

    puts

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -6,10 +6,10 @@
 */
 int main(void)
 {
-printf("Size of a char: %zu", sizeof(char));
-printf("Size of a int: %zu", sizeof(int));
-printf("Size of a long int: %zu", sizeof(long int));
-printf("Size of a long long int: %zu", sizeof(long long int));
-printf("Size of a float: %zu", sizeof(float));
+printf("Size of a char: %zu\n byte(s)", sizeof(char));
+printf("Size of a int: %zu\n byte(s)", sizeof(int));
+printf("Size of a long int: %zu\n byte(s)", sizeof(long int));
+printf("Size of a long long int: %zu\n byte(s)", sizeof(long long int));
+printf("Size of a float: %zu\n byte(s)", sizeof(float));
 return (0);
 }
