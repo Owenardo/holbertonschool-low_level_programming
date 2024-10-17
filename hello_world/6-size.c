04aaa80fe1a9d08e841b93e06455e5589e8cf5db
Owenardo <owenardm@gmail.com>
2024-10-17 16:36:47 +0000

    puts

---
+++ hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main - size of
+* Return: 0
+*
+*/
+int main(void)
+{
+printf("Size of a char: %zu", sizeof(char));
+printf("Size of a int: %zu", sizeof(int));
+printf("Size of a long int: %zu", sizeof(long int));
+printf("Size of a long long int: %zu", sizeof(long long int));
+printf("Size of a float: %zu", sizeof(float));
+return (0);
+}
