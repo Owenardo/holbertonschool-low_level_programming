3f0e29ce8f5b75b3b157728fc07acd50c7a3fba1
Owenardo <owenardm@gmail.com>
2024-10-21 12:56:35 +0000

    npz

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+* main - 16
+* Return: 0
+*
+*/
+int main(void)
+{
+int i = 0;
+int x = 'A';
+
+while (i <= 9)
+{
+putchar(i + 48);
+i++;
+}
+
+while (x <= 'F')
+{
+putchar(x + 32);
+x++;
+}
+putchar('\n');
+
+return (0);
+}
