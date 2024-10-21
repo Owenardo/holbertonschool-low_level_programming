09f7ab232446ca0d653795648f857d35ebfa7857
Owenardo <owenardm@gmail.com>
2024-10-21 13:33:24 +0000

    main.h

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* main - -putchar
+* Return: 0
+*
+*/
+int main(void)
+{
+char str[] = "_putchar";
+int i = 0;
+
+while (str[i] != '\0')
+{
+_putchar(str[i]);
+i++;
+}
+_putchar('\n');
+return (0);
+}
