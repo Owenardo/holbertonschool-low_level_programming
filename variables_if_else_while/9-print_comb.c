fadb348c683b3ab9980ace3ee6d66061598bcce3
Owenardo <owenardm@gmail.com>
2024-10-21 13:04:55 +0000

    npz

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+* main - single digit
+* Return: 0
+*
+*/
+int main(void)
+{
+int i = 0;
+
+while (i < 10)
+{
+putchar(i + 48);
+
+if (i < 9)
+{
+putchar(',');
+putchar(' ');
+}
+i++;
+}
+putchar('\n');
+
+return (0);
+}
