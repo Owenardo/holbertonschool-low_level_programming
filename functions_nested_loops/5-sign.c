368bd67d6b615a3e37831d7d3db815d4874eb66e
Owenardo <owenardm@gmail.com>
2024-10-23 12:55:40 +0000

    sign

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -2,24 +2,25 @@
 #include "main.h"
 /**
 *print_sign - return a sign
-* @n - number
-* return: -1 0 1
+* @n: number
+* Return: -1 0 1
 *
 */
 int print_sign(int n)
 {
-        if (n > 0)
-        {
-                _putchar('+');
-                return (1);
-        } else if (n == 0)
-        {
-                int zero = 0;
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+else if (n == 0)
+{
+int zero = 0;
 
-                _putchar(zero + 48);
-                return (0);
-        }
+_putchar(zero + 48);
+return (0);
+}
 
-        _putchar('-');
-        return (-1);
+_putchar('-');
+return (-1);
 }
