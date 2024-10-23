88aa6b6ea4f9d68decefe1baa06af847f4dad072
Owenardo <owenardm@gmail.com>
2024-10-23 14:36:58 +0000

    print_last

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
 #include "main.h"
+#include <limits.h>
 /**
 *print_last_digit - fuction
 * @n: is a number
 * Return: 0
 */
-
 int print_last_digit(int n)
 {
 int last_digit;
 
+if (n == INT_MIN)
+{
+_putchar('8');
+return (8);
+}
 if (n < 0)
 {
 n = -n;
