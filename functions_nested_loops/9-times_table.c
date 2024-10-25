ee670338462d2f5e1f2f563f5afe4d67d290659a
Owenardo <owenardm@gmail.com>
2024-10-25 12:36:50 +0000

    89

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -8,15 +8,15 @@
 void print_number(int n)
 {
 if (n < 0)
-	{
-		n = -n;
-		_putchar('-');
-	}
-	if (n >= 10)
-	{
-		print_number(n / 10);
-	}
-	_putchar('0' + (n % 10));
+{
+n = -n;
+_putchar('-');
+}
+if (n >= 10)
+{
+print_number(n / 10);
+}
+_putchar('0' + (n % 10));
 }
 /**
 * times_table - multiplies 9 table
