68563534a565074613e7ff732e2d99355bf450d8
Owenardo <Owenardm@gmail.com>
2024-10-29 12:59:00 +0000

    _

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* print_diagonal - fuction 
+* @n: number
+* 
+* Return: 0
+*/
+void print_diagonal(int n)
+{
+int i, j;
+if (n <= 0)
+{
+	_putchar('\n');
+}
+else
+{
+for (i = 0; 1 < n; i++)
+{
+for (j = 0; j < n; j++)
+{
+_putchar(' ');
+}
+_putchar('\\');
+		_putchar('\n');
+}
+}
+}
