4c374f921e19b921a3c0a99a269fb86d9a6c9662
Owenardo <Owenardm@gmail.com>
2024-10-30 11:41:29 +0000

    more

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* print_triangle - fuction print a triangle
+* @size: size
+* Return: 0
+*
+*/
+
+void print_triangle(int size)
+{
+	int c;
+	int f;
+
+	if (size > 0)
+	{
+		for (c = 0; c < size; c++)
+		{
+			for (f = 0; f < size; f++)
+		{
+			if
+				(f < size - c - 1)
+			{
+
+		_putchar(' ');
+			}
+
+		else
+			{
+				_putchar(35);
+			}
+		}
+		_putchar('\n');
+	}
+	}
+		else
+		{
+			_putchar(10);
+	}
+}
--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - fizz buzz function
- *
- *
- * Return: always 0
- */
+* main - fizz buzz function
+*
+*
+* Return: always 0
+*/
 
 int main(void)
 {
