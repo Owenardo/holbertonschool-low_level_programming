48890fc55afaa3ab5039317c26c9f61a85f15331
Owenardo <Owenardm@gmail.com>
2024-10-31 15:02:04 +0000

    pipi

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,8 +1,49 @@
+#include <limits.h>
 #include <stdio.h>
 #include "main.h"
-
+/**
+ * _atoi - fuction
+ * @s: string
+ * Return: syr
+ *
+ */
 int _atoi(char *s)
 {
-  printf("%d", *s);
-    return(0);
+int sign = 1;
+unsigned int result = 0;
+int found_digit = 0;
+
+while (*s)
+{
+if (*s == ' ')
+{
+s++;
+continue;
+}
+if (*s == '-')
+{
+sign = -sign;
+s++;
+}
+else if (*s == '+')
+{
+s++;
+}
+while (*s >= '0' && *s <= '9')
+{
+found_digit = 1;
+if (result > (UINT_MAX - (*s - '0')) / 10)
+{
+return ((sign == 1) ? INT_MAX : INT_MIN);
+}
+result = result * 10 + (*s - '0');
+s++;
+}
+if (found_digit)
+{
+break;
+}
+s++;
+}
+return (sign *result);
 }
