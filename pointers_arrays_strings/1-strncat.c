4703b1ca6e492c2330e859f93ba5dce518eb8066
Owenardo <owenardm@gmail.com>
2024-11-01 14:04:54 +0000

    0

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strncat - fuction that concatenates two strings
+ * @dest: string
+ * @src: string
+ * @n: bytes 
+ *
+ * Return: dest
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0, j;
+
+	while (dest[i] != '\0')
+		i++;
+	for (j = 0; j < n && src[j] != '\0'; j++, i++)
+		dest[i] = src[j];
+	dest[i] = '\0';
+	return (dest);
+}
