197f5bdf7e7605fc0cab75c9a14d6bb9e366f232
Owenardo <Owenardm@gmail.com>
2024-11-05 14:18:50 +0000

    1

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -8,11 +8,9 @@
 */
 int _strlen_recursion(char *s)
 {
-int length = 0;
-while (*s != '\0')
-{
-length++;
-s++;
-}
-return (length);
+	if (*s != '\0')
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+	return (0);
 }
