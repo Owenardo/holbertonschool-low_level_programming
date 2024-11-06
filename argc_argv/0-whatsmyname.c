95184a24ed7b2f1869e5f59a05b1027828162849
Owenardo <owenardm@gmail.com>
2024-11-06 14:12:05 +0000

    0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - fuction
+ *@argc: asd
+ *@argv: asdf
+ *Return: 0
+ */
+int main(int argc, char *argv[])
+{
+if (argc < 0)
+{
+printf("%s\n", argv[0]);
+}
+return (0);
+}
