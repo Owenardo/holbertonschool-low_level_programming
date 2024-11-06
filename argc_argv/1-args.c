4a142bb4fea6d74118b40bddd2272c990af5f34d
Owenardo <owenardm@gmail.com>
2024-11-06 14:26:36 +0000

    1

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
- *
- *
- *
- *
+ *main - prints the number of arguments
+ *@argc: asd
+ *@argv: dsa
+ *Return:0
  */
 int main(int argc, char *argv[])
 {
-  (void)argv;
+(void)argv;
 printf("%d\n", argc - 1);
-return 0;
+return (0);
 }
