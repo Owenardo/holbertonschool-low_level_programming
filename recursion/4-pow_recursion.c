6e65680e2d0e2e19ec405dce7d1dfd90a0b973af
Owenardo <Owenardm@gmail.com>
2024-11-06 11:54:32 +0000

    4

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -4,7 +4,7 @@
  * _pow_recursion - x to the power of y
  * @x: Number Integer
  * @y: Power
- * Return: On success 1. On error, -1 is returned, and errno is set appropriately.
+ * Return: On success 1. On error, -1 is returned.
  */
 int _pow_recursion(int x, int y)
 {
