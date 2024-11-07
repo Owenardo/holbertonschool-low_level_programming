dcce23e1159bfa0e8bb31b9b82ef96dd27525c38
Owenardo <owenardm@gmail.com>
2024-11-07 12:36:07 +0000

    mul

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - argv 1 * 2
+ *@argc: asd
+ *@argv: dasd
+ *Return: 1 error or 0 total
+ */
+int main(int argc, char *argv[])
+{
+int total;
+
+if (argc < 2)
+{
+printf("error\n");
+return (1);
+}
+total = atoi(argv[1]) * atoi(argv[2]);
+printf("%d\n", total);
+return (0);
+}
