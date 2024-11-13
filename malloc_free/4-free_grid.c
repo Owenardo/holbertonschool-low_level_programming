866b1b96fc5d374f05dd863a52673e5d63bae92c
Owenardo <Owenardm@gmail.com>
2024-11-13 22:05:48 +0000

    a

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -1,9 +1,9 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * free_grid - free a 2-dimensional array
- * @grid: pointer
- * @height: rows
+* free_grid - free a 2-dimensional array
+* @grid: pointer
+* @height: rows
  * Return: void
  */
 void free_grid(int **grid, int height)
