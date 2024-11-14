2f60bdc8291268754ee80cbf45f737515669fab8
Owenardo <Owenardm@gmail.com>
2024-11-14 14:44:30 +0000

    s

---
+++ structures_typedef/dog.h
@@ -0,0 +1,19 @@
+
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct my_dog - structure
+ * @owner: name of the owner
+ * @name: name of the dog
+ * @age: age of dog
+ *
+ * Description: Structure to create a dog
+ */
+typedef struct dog
+{
+	char *owner;
+	char *name;
+	float age;
+}  my_dog;
+#endif
