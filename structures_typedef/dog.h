1de7dff809060195ed107bc5b2f47f80f710ebb2
Owenardo <Owenardm@gmail.com>
2024-11-14 15:21:32 +0000

    3

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -12,9 +12,9 @@
 typedef struct dog
 {
 	char *owner;
-	char *name;
+char *name;
 	float age;
-} my_dog;
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
