a42c6746725e27018874682670c7e6b00e6b00da
Owenardo <owenardm@gmail.com>
2024-12-07 12:49:00 +0000

    0

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -11,16 +11,16 @@ unsigned int result = 0;
 int i = 0;
 if (b == NULL)
 {
-return 0;
+return (0);
 }
 for (; b[i] != '\0'; i++)
 {
 if (b[i] != '0' && b[i] != '1')
 {
-return 0;
+return (0);
 }
 result = result * 2 + (b[i] - '0');
 }
 
-return result;
+return (result);
 }
